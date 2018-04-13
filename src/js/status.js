2cd31938a69c13711449c7ccc2c6ab0fa916d959
Joseph Hill <joseph.hill1@outlook.com>
2018-04-13 13:00:35 -0400

    Added methods to filter by profession and age as well as methods to chart the filtered data

--- src/js/status.js
+++ src/js/status.js
@@ -31,6 +31,8 @@ class Status {
         this.$tabArea.classList.remove('hidden');
         this.$chartArea.classList.remove('hidden');
         this.loadExperience();
+        this.loadProfession();
+        this.loadAge();
       })
       .catch(() => {
         this.$loadingIndicator.classList.add('hidden');
@@ -41,17 +43,32 @@ class Status {
   processData(data) {
     let filteredData = [];
     for (let entry in data) if(data.hasOwnProperty(entry)) filteredData.push(data[entry]);
-    console.log(filteredData);
     return filteredData;
+  }
+
+  filterByExp(data) {
+    let beginner = 0, intermediate = 0, advanced = 0;
+    data.forEach((entry) => {
+      switch (entry.experience) {
+        case 1:
+          beginner += 1;
+          break;
+        case 2:
+          intermediate += 1;
+          break;
+        case 3:
+          advanced += 1;
+          break;
+        default:
+      }
+    });
+    return [beginner, intermediate, advanced];
   }
 
   loadExperience() {
     const data = {
       datasets: [{
-        data: this.statisticData.map((entry) => {
-          console.log(entry);
-          return entry.experience;
-        }),
+        data: this.filterByExp(this.statisticData),
         backgroundColor:[
           'rgba(255, 99, 132, 0.6)',
           'rgba(54, 162, 235, 0.6)',
@@ -75,6 +92,104 @@ class Status {
     });
 
     console.log(data.datasets);
+  }
+
+  filterbyProf(data) {
+    let schoolStudent = 0, collegeStudent = 0, trainee = 0, employee = 0;
+    data.forEach((entry) => {
+      switch (entry.profession) {
+        case 'school':
+          schoolStudent += 1;
+          break;
+        case 'college':
+          collegeStudent += 1;
+          break;
+        case 'trainee':
+          trainee += 1;
+          break;
+        case 'employee':
+          employee += 1;
+          break;
+        default:
+      }
+    });
+    return [schoolStudent, collegeStudent, trainee, employee];
+  }
+
+  loadProfession() {
+    const data = {
+      datasets: [{
+        data: this.filterbyProf(this.statisticData),
+        backgroundColor:[
+          'rgba(255, 99, 132, 0.6)',
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(255, 206, 86, 0.6)',
+          'rgba(75, 192, 192, 0.6)',
+        ],
+        borderColor: [
+          'white',
+          'white',
+          'white',
+          'white',
+        ]
+      }],
+      labels: [
+        'School Students',
+        'College Students',
+        'Trainees',
+        'Employees'
+      ]
+    };
+    new Chart(this.$professionCanvas,{
+      type: 'pie',
+      data,
+    });
+  }
+
+  filterByAge(data) {
+    let tenToFifteen = 0, fifteenToTwenty = 0, twentyToTwentyFive = 0;
+    data.forEach((entry) => {
+      switch (true) {
+        case (entry.age > 9 && entry.age < 16):
+          tenToFifteen += 1;
+          break;
+        case (entry.age > 14 && entry.age < 21):
+          fifteenToTwenty += 1;
+          break;
+        case (entry.age >= 21):
+          twentyToTwentyFive += 1;
+          break;
+        default:
+      }
+    });
+    return [tenToFifteen, fifteenToTwenty, twentyToTwentyFive];
+  }
+
+  loadAge() {
+    const data = {
+      datasets: [{
+        data: this.filterByAge(this.statisticData),
+        backgroundColor:[
+          'rgba(255, 99, 132, 0.6)',
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(255, 206, 86, 0.6)',
+        ],
+        borderColor: [
+          'white',
+          'white',
+          'white',
+        ]
+      }],
+      labels: [
+        '10-15 years',
+        '15-20 years',
+        '20-25 years'
+      ]
+    };
+    new Chart(this.$ageCanvas,{
+      type: 'pie',
+      data,
+    });
   }
 }
 
